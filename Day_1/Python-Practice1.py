89d811f64b162024731e9d54b936a0f54b4fdd5a
GrayGhost99 <88752485+GrayGhost99@users.noreply.github.com>
2022-03-26 18:12:07 -0400

    first commit

---
+++ Day_1/Python-Practice1.py
@@ -0,0 +1,12 @@
+# Swap the values for input a and b for the output
+a = input("a: ")
+b = input("b: ")
+
+# Code added to swap values around by adding a third variable
+c = a
+a = b
+b = c
+
+# Output
+print("a: " + a)
+print("b: " + b)
